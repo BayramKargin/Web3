3ff4f7e9ca0a664ed123fe51f6578806bdbc1621
BayramKargin <bayram442814@gmail.com>
2024-05-26 12:04:16 +0300

    chat

--- lib/chat.ts
+++ lib/chat.ts
@@ -1,18 +1,19 @@
 import web3 from './web3';
 import Chat from '../chat-blockchain/build/contracts/Chat.json';
 
-const address = '0xcDA1B00420deb49b3A71EF1f603740933De6a0Ed'; // Replace with your contract address
+const address = '0x4cA678d4E27098f8bcf233b848D2b3b995Ff8818'; // Replace with your contract address
 const instance = new web3.eth.Contract(Chat.abi as any, address);
 
+
 interface BlockchainMessage {
-    sender: string;
-    text: string;
-    timestamp: number;
-  }
-  
-  export const getMessages = async (): Promise<BlockchainMessage[]> => {
-    const messages: BlockchainMessage[] = await instance.methods.getMessages().call();
-    return messages;
-  };
+  sender: string;
+  text: string;
+  timestamp: number;
+}
+
+export const getMessages = async (): Promise<BlockchainMessage[]> => {
+  const messages: BlockchainMessage[] = await instance.methods.getMessages().call();
+  return messages;
+};
 
 export default instance;
